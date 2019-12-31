a98048f7d10f3f53862ef47464a97e1598042d75
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-01-01 00:23:01 +0530

    Add files via upload

---
+++ happy.sh
@@ -0,0 +1,14 @@
+Happy () {
+	figlet Happy New|lolcat
+	figlet 	Year|lolcat 
+	
+printf "\n\n\033[92m	<<<<====>>>> Conguratulation !!! <<<<====>>>> \n\n"
+sleep 0.50
+New
+}
+New () {
+printf "   	<<<===>>> Happy New Year 2020 <<<===>>> \n\n"|lolcat
+sleep 0.40
+Happy
+}
+Happy
