541ed468218993e6646e5b7b8ebd830d8005601f
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-01-01 21:23:44 +0530

    Add files via upload

---
+++ happy2.py
@@ -0,0 +1,34 @@
+import os
+import random
+def a():
+	os.system("figlet Happy New|lolcat")
+	os.system("figlet 	Year|lolcat")
+	os.system("echo")
+	os.system('echo "$( date +"%c" | toilet -f term -F gay -F border )"')
+	os.system("echo")
+	os.system("cowsay Happy New Year 2020 | lolcat")
+	os.system('printf "\n\n\033[92m	<<<<====>>>> Conguratulation !!! <<<<====>>>> \n\n"')
+	os.system('printf "   	<<<===>>> Happy New Year 2020 <<<===>>> \n\n"|lolcat')
+	os.system('printf "\n\n Happy new year 2020 for all friends from rootedcyber 🎉🎈🎈🎈🎈\n\n\n"|toilet -f term -F gay')
+	os.system('printf "\n\n2 O 2 O || 2 O 2 O || 2 O 2 O || 2 O 2 O\n\n"|lolcat')
+	
+	
+	
+	
+os.system('printf "Happy new year 🎉🎈🎈🎈🎈\n\n"|lolcat')
+a()
+os.system('printf "2 O 2 O || 2 O 2 O || 2 O 2 O || 2 O 2 O\n"|lolcat')
+os.system('printf "Happy new year 🎉🎈🎈🎈🎈\n\n"|lolcat')
+os.system('printf "2 O 2 O || 2 O 2 O || 2 O 2 O || 2 O 2 O\n"|lolcat')
+os.system('printf "Happy new year 🎉🎈🎈🎈🎈\n\n"|lolcat')
+os.system('printf "2 O 2 O || 2 O 2 O || 2 O 2 O || 2 O 2 O\n"|lolcat')
+os.system('printf "Happy new year 🎉🎈🎈🎈🎈\n\n"|lolcat')
+os.system('printf "2 O 2 O || 2 O 2 O || 2 O 2 O || 2 O 2 O\n"|lolcat')
+os.system('printf "Happy new year 🎉🎈🎈🎈🎈\n\n"|lolcat')
+os.system('printf "2 O 2 O || 2 O 2 O || 2 O 2 O || 2 O 2 O\n"|lolcat')
+os.system('printf "Happy new year 🎉🎈🎈🎈🎈\n\n"|lolcat')
+os.system('printf "2 O 2 O || 2 O 2 O || 2 O 2 O || 2 O 2 O\n"|lolcat')
+os.system('printf "Happy new year 🎉🎈🎈🎈🎈\n\n"|lolcat')
+os.system('printf "2 O 2 O || 2 O 2 O || 2 O 2 O || 2 O 2 O\n"|lolcat')
+os.system('printf "Happy new year 🎉🎈🎈🎈🎈\n\n"|lolcat')
+a()
