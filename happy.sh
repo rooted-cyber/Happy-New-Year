291d70fd584e49aee7ed7b766bf3313e115c5945
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2020-01-01 12:30:52 +0530

    Add files via upload

--- happy.sh
+++ happy.sh
@@ -3,11 +3,34 @@ if [ -e figlet ];then
 echo
 else
 pkg install figlet
+fi
+if [ -e lolcat ];then
+echo
+else
+apt-get install python
 pip install lolcat
+clear
+fi
+cd $PREFIX/bin
+if [ -e cowsay ];then
+echo
+else
+pkg install cowsay
+clear
+fi
+if [ -e toilet ];then
+echo
+else
+pkg install toilet
+clear
 fi
 Happy () {
 	figlet Happy New|lolcat
 	figlet 	Year|lolcat 
+	echo
+	echo "$( date +"%c" | toilet -f term -F gay -F border )"
+	echo
+	cowsay Happy New Year 2020 | lolcat
 	
 printf "\n\n\033[92m	<<<<====>>>> Conguratulation !!! <<<<====>>>> \n\n"
 sleep 0.50
@@ -15,7 +38,7 @@ New
 }
 New () {
 printf "   	<<<===>>> Happy New Year 2020 <<<===>>> \n\n"|lolcat
-sleep 0.40
+sleep 5
 Happy
 }
 Happy
